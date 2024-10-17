4d417d320cdd42856333b7ae58ded449a8f7ba75
Henry Wu <h27wu@uwaterloo.ca>
2024-10-16 20:13:57 -0400

    Create project

---
+++ ESP_Salt_Level_Monitor/ESP_Salt_Level_Monitor.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+const char* SSID;
+const char* PASSWORD;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("network-name", "pass-to-network");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
