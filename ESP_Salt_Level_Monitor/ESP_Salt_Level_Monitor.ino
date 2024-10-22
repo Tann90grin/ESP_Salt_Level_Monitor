0e9d4957ebee07809292f467903e93d2627d9144
Henry Wu <h27wu@uwaterloo.ca>
2024-10-21 20:04:10 -0400

    Basic setup for sensor and wifi driver

--- ESP_Salt_Level_Monitor/ESP_Salt_Level_Monitor.ino
+++ ESP_Salt_Level_Monitor/ESP_Salt_Level_Monitor.ino
@@ -1,12 +1,17 @@
 #include <ESP8266WiFi.h>
 const char* SSID;
 const char* PASSWORD;
+const int trigPin = 13;
+const int echoPin = 12;
+long duration;
+float distanceCM;
+#define SOUND_VELOCITY 0.034
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-
+  WiFi.hostname("ESP-host");
   WiFi.begin("network-name", "pass-to-network");
 
   Serial.print("Connecting");
@@ -19,9 +24,23 @@ void setup()
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCM = duration * SOUND_VELOCITY / 2;
+  Serial.print("Distance: ");
+  Serial.print(distanceCM);
+  Serial.println("cm");
+  delay(1000);
 }
